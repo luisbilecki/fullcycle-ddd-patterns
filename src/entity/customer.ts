894bec92e783670342ff61788dfa8f7aba5ee415
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-11-30 21:32:39 -0300

    feat: add business rules

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -3,30 +3,25 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string, address: string, active: boolean) {
         this._id = id;
         this._name = name;
         this._address = address;
+        this._active = active;
     }
     
-     get id() : string {
-        return this._id;
-    }
-    
-    get name() : string {
-        return this._name;
-    }
-    
-    get address() : string {
-        return this._address;
+    changeName(name: string) {
+        this._name = name
     }
 
-    set name(name: string) {
-        this._name = name;
+    activate() {
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
     }
     
-    set address(address: string) {
-        this._address = address;
-    }
 }
