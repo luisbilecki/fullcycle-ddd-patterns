45b145b3b571f8dd0043b61ebbfa03c82492537d
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-12-07 23:34:02 -0300

    feat: adding relation in order to product

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -6,27 +6,36 @@ describe("Order unit tests", () => {
     it("should throw error when id is empty", () => {
         
         expect(() => {
-            new Order("", "123", [])
+            new Order("", "123", []);
         }).toThrowError("Id is required");
     })
 
     it("should throw error when customer id is empty", () => {
         
         expect(() => {
-            new Order("1", "", [])
+            new Order("1", "", []);
         }).toThrowError("Customer Id is required");
     })
 
     it("should throw error when order has items", () => {
         
         expect(() => {
-            new Order("1", "222", [])
+            new Order("1", "222", []);
         }).toThrowError("Items are required");
     })
 
+    it("should throw error when order has item with quantity zero", () => {
+        const item1 = new OrderItem("i1", "Item 1", "p1", 10, 1);
+        const item2 = new OrderItem("i1", "Item 1", "p2", 50, 0);
+
+        expect(() => {
+            new Order("o1", "c1", [item1, item2]);
+        }).toThrowError("Quantity must be greater than 0");
+    })
+
     it("should calculate total", () => {
-        const item1 = new OrderItem("i1", "Item 1", 10, 1);
-        const item2 = new OrderItem("i1", "Item 1", 50, 2);
+        const item1 = new OrderItem("i1", "Item 1", "p1", 10, 1);
+        const item2 = new OrderItem("i1", "Item 1", "p2", 50, 2);
         const order1 = new Order("o1", "c1", [item1]);
         let total = order1.total();
         
