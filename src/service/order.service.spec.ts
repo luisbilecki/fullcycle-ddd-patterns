8ebd1e4d0e4d39075d3b8384873131d8566759fd
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-12-13 19:23:05 -0300

    feat: define reward policies

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,20 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
 
 describe("Order service unit tests", () => {
+
+    it("should place an order", () => {
+
+        const customer1 = new Customer("c1", "Customer 1");
+        const item1 = new OrderItem("i1", "p1", 10, 1);
+
+        const order = OrderService.placeOrder(customer1, [item1]);
+
+        expect(customer.rewardPoints).toBe(5);
+        expect(order.total()).toBe(10);
+    });
 
     it("should get total of all orders", () => {
     
