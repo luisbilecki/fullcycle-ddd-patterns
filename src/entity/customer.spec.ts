7a13e293e5d42629ffff13199753a52d0e80aa93
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-12-13 19:35:23 -0300

    feat: add a test case to check reward points on customer spec

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -49,4 +49,14 @@ describe("Customer unit tests", () => {
         expect(customer.isActive()).toBe(false);
     });
 
+    it("should add reward points", () => {
+        const customer = new Customer("123", "Luis");
+        expect(customer.rewardPoints).toBe(0);
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(10);
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(20);
+    });
 });
