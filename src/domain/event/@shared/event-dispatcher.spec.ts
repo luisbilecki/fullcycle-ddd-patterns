e5f065eb953122917485c4e8871242912bef2128
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-01-09 20:33:56 -0300

    feat: add register event handler method

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,3 +1,6 @@
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
+import EventDispatcher from "./event-dispatcher";
+
 describe("Domain events tests", () => {
 
     it("should register an event handler", () => {
@@ -6,7 +9,6 @@ describe("Domain events tests", () => {
         const eventHandler = new SendEmailWhenProductIsCreatedHandler();
 
         eventDispatcher.register("ProductCreatedEvent", eventHandler);
-
 
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toBe(1);
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address("Rua dois", 2, "12345-678", "São Paulo");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "p1", 10, 1);
-const item2 = new OrderItem("2", "p2", 15, 1);
+const item1 = new OrderItem("1", "product 1", "p1", 10, 1);
+const item2 = new OrderItem("2", "product 2", "p2", 15, 1);
 
 const order = new Order("1", "123", [item1, item2]);
