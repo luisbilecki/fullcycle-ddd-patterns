32b771f9f9e4cc17c5a401c4b09e9683821d0f46
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-01-12 21:29:00 -0300

    feat: modularize infrastructure

--- src/infrastructure/order/repository/sequelize/order-item.model.ts
+++ src/infrastructure/order/repository/sequelize/order-item.model.ts
@@ -6,9 +6,9 @@ import {
     ForeignKey,
     BelongsTo,
   } from "sequelize-typescript";
-  import CustomerModel from "./customer.model";
+  import CustomerModel from "../../../customer/repository/sequelize/customer.model";
   import OrderModel from "./order.model";
-  import ProductModel from "./product.model";
+  import ProductModel from "../../../product/repository/sequelize/product.model";
   
   @Table({
     tableName: "order_items",
